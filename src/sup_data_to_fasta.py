fcb2e600c6b33fe0644550508152fddc6a44c25e
mtsz-thiago <mtsz.thiago@gmail.com>
2024-02-06 18:53:18 +0000

    original flattened data joined

--- src/sup_data_to_fasta.py
+++ src/sup_data_to_fasta.py
@@ -7,13 +7,7 @@ from pathlib import Path
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
-from Bio.SeqFeature import SeqFeature, FeatureLocation
-from Bio.Blast import NCBIWWW, NCBIXML
 import uuid
-
-def deduplicate_queries_by_seq(df):
-    duplicated_seqs = df.seq.duplicated()
-    return df[~duplicated_seqs]
 
 def flatten_df(df):
     
@@ -35,8 +29,7 @@ def flatten_df(df):
 
 def extract_queries_and_expected(df):
     flattened_df = flatten_df(df)
-    deduplicated_df = deduplicate_queries_by_seq(flattened_df)
-    with_query_id = add_query_id_column(deduplicated_df)
+    with_query_id = add_query_id_column(flattened_df)
     bio_df = map_to_fasta(with_query_id)
     return bio_df, with_query_id
 
@@ -44,13 +37,13 @@ def map_raw_data_to_queries_and_expected(df_dict):
     queries_dict = {}
     expected_dict = {}
     for k, df in df_dict.items():
-        bio_df, expected_df = extract_queries_and_expected(df)
+        bio_df, expected = extract_queries_and_expected(df)
         queries_dict[k] = bio_df
-        expected_dict[k] = expected_df
-    return queries_dict, expected_dict
+        expected_dict[k] = expected
+    return queries_dict, expected_dict      
 
 def add_query_id_column(df):
-    df['query_id'] = df.apply(lambda x: str(uuid.uuid1()), axis=1)
+    df['query_id'] = df.groupby('seq').name.transform(lambda x: uuid.uuid4().hex)
     return df
 
 def load_xlsx(input_file):
@@ -59,23 +52,18 @@ def load_xlsx(input_file):
     return sup_dict
 
 def map_to_fasta(df):
-    return df.apply(axis=1, func=map_record_to_SeqRecord)
+    bio_df = df.groupby('seq').apply(lambda x: map_record_to_SeqRecord(x), include_groups=False)
+    return bio_df
 
 def map_record_to_SeqRecord(r):
-    strand_code = 1 if r[f"Strand"] == "+" else -1
-    id = r['query_id']
-    name = id
-    seq = Seq(r[f"seq"])
-    from_pos = r[f"from"]
-    to_pos = r[f"to"]
-    type = r[f"type"]
-    description = f"{id} origin: {r.origin} expected {r.name}"
+    id = f"{r.query_id.iloc[0]}"
+    # print(id)
+    name = ''
+    seq = Seq(r.name)
+    description = f"{id} from {r.chimera_idx.values}"
     return SeqRecord(
         id=id,
         description=description,
-        features=[
-                SeqFeature(FeatureLocation(from_pos, to_pos, strand=strand_code),type=type)
-            ],
         name=name, 
         seq=seq)
 
@@ -101,7 +89,6 @@ def main(input_file, output_dir):
     queries_dict, expected_dict = map_raw_data_to_queries_and_expected(sup_data_dict)
 
     write_csvs(queries_dict, expected_dict, output_dir)
-   
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert supplementary data to FASTA format.")
